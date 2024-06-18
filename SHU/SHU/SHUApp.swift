684e147458711be1fee26525e00059cfda794cfb
Paul <121603347+Paul-inPohang@users.noreply.github.com>
2024-06-18 12:33:44 +0900

    [Create] Initialize Project "SHU"

---
+++ SHU/SHU/SHUApp.swift
@@ -0,0 +1,17 @@
+//
+//  SHUApp.swift
+//  SHU
+//
+//  Created by 김현수 on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SHUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
