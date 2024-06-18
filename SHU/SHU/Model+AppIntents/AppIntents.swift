2a9740c83e03d204d4394a9793b6681efcecb0f4
Paul <121603347+Paul-inPohang@users.noreply.github.com>
2024-06-19 06:14:03 +0900

    [Feat: AppShortCuts 구문 등록]
    -수유시작:
    "\(앱이름) 수유 시작~"
    "\(앱이름) 맘마 먹자~"
    
    -수유종료
    "\(앱이름) 수유 종료~"
    "\(앱이름) 다 먹었다~"

--- SHU/SHU/Model+AppIntents/AppIntents.swift
+++ SHU/SHU/Model+AppIntents/AppIntents.swift
@@ -12,8 +12,6 @@ struct AddFeedingIntent: AppIntent {
     static let title: LocalizedStringResource = "수유 시작"
     
     func perform() async throws -> some IntentResult & ProvidesDialog {
-//        let manager = FeedingManager.shared
-//        manager.startFeeding(startTime: Date())
             FeedingManager.shared.startFeeding(startTime: Date())
         return .result(dialog: "수유를 시작합니다")
     }
@@ -29,10 +27,27 @@ struct EndFeedingIntent: AppIntent {
             let alarmTime = Calendar.current.date(byAdding: .minute, value: 1, to: Date())!
             AlarmManager.shared.scheduleAlarm(at: alarmTime, withTitle: "수유시간", andBody: "아기 밥먹일 시간이에요!")
         }
-//        let feedingManager = FeedingManager.shared
-//        feedingManager.endFeeding(endTime: Date())
-//        let alarmManager = AlarmManager.shared
-//        alarmManager.scheduleAlarm(after: 5, withTitle: "수유시간", andBody: "아기 밥먹일 시간이에요!")
         return .result(dialog: "수유를 종료하고, 3시간 뒤 알람을 설정했어요!")
     }
 }
+
+//단축어 자동 제공
+struct FeedingShortcuts: AppShortcutsProvider {
+    static var appShortcuts: [AppShortcut] {
+        AppShortcut(
+            intent: AddFeedingIntent(),
+            phrases: ["\(.applicationName) 수유 시작",
+                      "\(.applicationName) 맘마 먹자"],
+            shortTitle: "수유 시작",
+            systemImageName: "calendar"
+        )
+        AppShortcut(
+            intent: EndFeedingIntent(),
+            phrases: ["\(.applicationName) 수유 종료",
+                      "\(.applicationName) 다 먹었다"],
+            shortTitle: "분유 종료",
+            systemImageName: "calendar"
+        )
+    }
+}
+
